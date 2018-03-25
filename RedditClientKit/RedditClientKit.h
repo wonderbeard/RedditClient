01c7a59b322f00cfbc5f3c8506ca5e3059c23d6f
Andrew Malyarchuk <andrew.malyarchuk@gmail.com>
2018-03-25 21:19:01 +0300

    add playground to the workspace

---
+++ RedditClientKit/RedditClientKit.h
@@ -0,0 +1,19 @@
+//
+//  RedditClientKit.h
+//  RedditClientKit
+//
+//  Created by Andrew Malyarchuk on 25.03.2018.
+//  Copyright © 2018 wonderbeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RedditClientKit.
+FOUNDATION_EXPORT double RedditClientKitVersionNumber;
+
+//! Project version string for RedditClientKit.
+FOUNDATION_EXPORT const unsigned char RedditClientKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedditClientKit/PublicHeader.h>
+
+
